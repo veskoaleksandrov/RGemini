b5720c0cd157093a5b5faedcb59d40cd4dd4b8db
Vesko <vesko@mechka.lan>
2018-05-27 08:34:27 -0400

    code clean-up

--- RGemini.R
+++ RGemini.R
@@ -117,45 +117,48 @@ stopifnot(library(httr, logical.return = TRUE))
 if (!require("plyr")) install.packages("plyr", repos = "https://cloud.r-project.org/")
 stopifnot(library(plyr, logical.return = TRUE))
 
-from <- "2017-04-01"
-from <- as.character(input$Max_Day)
+# Define time for report
+from <- as.character(Sys.Date() - 3)
 to <- as.character(Sys.Date() - 1)
-setwd("c:/Users/alteryx/Desktop/Alteryx Projects/Yahoo/")
-
 if(from >= to) {
   stop("'from' is greater than or equal to 'to'")
 }
 
+setwd("O:/Projects/Yahoo")
 
 #----------------------- Configuration section ----------------------------------------------------
 # 
 # 1. Find OAuth settings for yahoo:
-# Endpoints described here... 
+# Endpoints described here 
 # https://developer.yahoo.com/oauth2/guide/flows_authcode/
 yahoo <- oauth_endpoint(request = "get_token",                       
                         authorize = "request_auth", 
                         access = "get_token",
                         base_url = "https://api.login.yahoo.com/oauth2")
 
-# 2. Register an application at...
+# 2. Register an application at
 # https://developer.apps.yahoo.com/dashboard/createKey.html
 # Insert your values below - if secret is omitted, it will look it up in
-#    the YAHOO_CONSUMER_SECRET environmental variable.
-myapp <- list(lulu = oauth_app(appname = "yahoo", 
+# the YAHOO_CONSUMER_SECRET environmental variable.
+# 
+# In this example we are creating two applications
+# First for a fictional company called Contoso
+# Second for a fictional company called ACME
+myapp <- list(contoso = oauth_app(appname = "yahoo", 
                                key = "", 
                                secret = "", 
                                redirect_uri = "oob"), 
-              ib = oauth_app(appname = "yahoo", 
+              acme = oauth_app(appname = "yahoo", 
                              key = "", 
                              secret = "", 
                              redirect_uri = "oob"))
 
 # keep authorization codes updated
-authorizationCode <- list(lulu = "gsec6ja", 
-                          ib = "bepu7w4")
+authorizationCode <- list(contoso = "", 
+                          acme = "")
 
-refresh_token <- list(lulu = readRDS(file = "C:/Users/alteryx/Desktop/Alteryx Projects/Yahoo/Authorisation Tokens/gemini-lulu.rds"), 
-                      ib = readRDS(file = "C:/Users/alteryx/Desktop/Alteryx Projects/Yahoo/Authorisation Tokens/gemini-ib.rds"))
+refresh_token <- list(contoso = readRDS(file = "O:/Path/To/Your/Authorisation/Token/gemini-contoso.rds"), 
+                      acme = readRDS(file = "O:/Path/To/Your/Authorisation/Token/gemini-acme.rds"))
 
 report <- NULL
 for(t in 1:length(refresh_token)) {
@@ -173,6 +176,7 @@ for(t in 1:length(refresh_token)) {
   # loop through each advertiser
   for(a in 1:nrow(advertiser)) {
     print(paste0(Sys.time(), " - Advertizer: ", advertiser$advertiserName[a]))
+    # Request report from API
     requestReport <- geminiRequestReport(advertiserId = advertiser$id[a],
                                          from = from, 
                                          to = to, 
@@ -181,6 +185,7 @@ for(t in 1:length(refresh_token)) {
                                          access_token = my_token$access_token)
     print(paste0(Sys.time(), " - Job ID: ", requestReport$response$jobId))
     
+    # Wait for API to create report
     repeat({
       statusReport <- geminiGetReportStatus(jobId = requestReport$response$jobId, 
                                             advertiserId = advertiser$id[a], 
@@ -194,11 +199,13 @@ for(t in 1:length(refresh_token)) {
       Sys.sleep(15)
     })
     
+    # Read report 
     print(paste0(Sys.time(), " - Download report: ", statusReport$response$jobResponse))
     temp <- read.csv(file = statusReport$response$jobResponse, 
                      stringsAsFactors = FALSE)
     temp$Day <- as.Date(temp$Day)
     
+    # Save to CSV
     fileName <- paste0("yahoo_", advertiser$id[a], "_", from, "_", to, ".csv")
     print(paste0(Sys.time(), " - Write report: ", fileName))
     write.csv(x = temp, 
@@ -206,10 +213,8 @@ for(t in 1:length(refresh_token)) {
               row.names = FALSE, 
               fileEncoding = "UTF-8")
     
-    # append the report 
+    # Union Contoso and ACME reports
     print(paste0(Sys.time(), " - Appending ", nrow(temp), " row(s)."))
     report <- rbind.fill(report, temp)
   }
 }
-
-write.Alteryx(report, 1)
